d4928d5c4d2b7013b58e91506cfd1c428eb9033a
heyliceeee <58340504+heyliceeee@users.noreply.github.com>
2022-09-28 15:16:03 +0100

    create product viewmodel and find restaurant by ID

--- api/api/Controllers/Controller.cs
+++ api/api/Controllers/Controller.cs
@@ -80,5 +80,69 @@ namespace api.Controllers
         #endregion
 
         #endregion
+
+
+        #region TABLE PRODUCT
+
+        #region INSERT
+
+        [HttpPost]
+        [Route("insertProduct")]
+
+        public string AddProduct([FromBody] ProductViewModel product)
+        {
+            try
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Connection = conn;
+                command.CommandText = string.Format(@"  INSERT INTO product (category, name, description, image, price, nStars, idRestaurant, allergens, mainIngredients, nutritionalValue)
+                                                        VALUES (@category, @name, @description, @image, @price, @nStars, @idRestaurant, @allergens, @mainIngredients, @nutritionalValue)");
+
+                command.Parameters.AddWithValue("@category", ((object)product.category ?? DBNull.Value));
+                command.Parameters.AddWithValue("@name", product.name);
+                command.Parameters.AddWithValue("@description", ((object)product.description ?? DBNull.Value));
+                command.Parameters.AddWithValue("@image", ((object)product.image ?? DBNull.Value));
+                command.Parameters.AddWithValue("@price", product.price);
+                command.Parameters.AddWithValue("@nStars", ((object)product.nStars ?? DBNull.Value));
+                command.Parameters.AddWithValue("@idRestaurant", ((object)product.idRestaurant ?? DBNull.Value));
+                command.Parameters.AddWithValue("@allergens", ((object)product.allergens ?? DBNull.Value));
+                command.Parameters.AddWithValue("@mainIngredients", ((object)product.mainIngredients ?? DBNull.Value));
+                command.Parameters.AddWithValue("@nutritionalValue", ((object)product.nutritionalValue ?? DBNull.Value));
+
+                int i = command.ExecuteNonQuery();
+
+                conn.Close();
+
+                if (i == 1) //if insert product
+                {
+                    var testTuple = (StatusCode: 200, Message: "Produto inserido com sucesso");
+
+                    return JsonConvert.SerializeObject(new { testTuple.StatusCode, testTuple.Message });
+                }
+                else
+                {
+                    var testTuple2 = (StatusCode: 404, Message: "Restaurante inserido sem sucesso");
+
+                    return JsonConvert.SerializeObject(new { testTuple2.StatusCode, testTuple2.Message });
+                }
+            }
+            catch (Exception ex)
+            {
+                var testTuple3 = (StatusCode: 404, Message: ex.Message);
+
+                return JsonConvert.SerializeObject(new { testTuple3.StatusCode, testTuple3.Message });
+            }
+
+
+            var testTuple4 = (StatusCode: 404, Message: "Produto inserido sem sucesso");
+
+            return JsonConvert.SerializeObject(new { testTuple4.StatusCode, testTuple4.Message });
+        }
+
+        #endregion
+
+        #endregion
     }
 }
---
+++ api/api/ViewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int idProduct { get; set; }
+        public string category { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+        public decimal price { get; set; }
+        public int nStars { get; set; }
+        public int idRestaurant { get; set; }
+        public string allergens { get; set; }
+        public string mainIngredients { get; set; }
+        public string nutritionalValue { get; set; }
+    }
+}
